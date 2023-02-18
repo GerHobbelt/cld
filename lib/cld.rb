8e1fd1da4eb397158acf634152ae3ba6b926fb03
Maxim Sadovsky <maksim@sadovskij.com>
2023-02-18 20:13:35 +0200

    Fix for ruby 3.2

--- lib/cld.rb
+++ lib/cld.rb
@@ -10,8 +10,7 @@ module CLD
   else
     FFI::Platform::LIBSUFFIX
   end
-
-  ffi_lib File.join(File.expand_path(File.dirname(__FILE__)), '..', 'ext', 'cld', 'libcld2.' + suffix)
+  ffi_lib File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib', 'libcld2.' + suffix)
 
   def self.detect_language(text, is_plain_text=true)
     result = detect_language_ext(text.to_s, is_plain_text)
